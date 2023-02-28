87cfe53e15756a64d2ab149a3006887a93a9983a
Marcos <marcos.sanchez@saei.mx>
2023-02-27 23:17:59 -0600

    Comments. Logic inside curly brackets.

--- first-react-app/src/index.jsx
+++ first-react-app/src/index.jsx
@@ -1,8 +1,19 @@
-import './style.css'
-import { createRoot } from 'react-dom/client'
+import "./style.css";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector("#root"));
+const name = "Marcos";
 
 root.render(
-    <h1>Hello React</h1>
-)
+  // ! A estas llaves sin tag se les llama "fragments"
+
+  <>
+    <h1 className="title">Hello React</h1>
+    <input type="checkbox" name="" id="the_checkbox" />
+    {/* Esto es un comentario en JSX */}
+
+    <label htmlFor="the_checkbox">dame click :) </label>
+    <h1>{name}</h1>
+    <h1>{name ? "yes" : "no"}</h1>
+  </>
+);
